78a4f440b374be251222f14aa09c3bfe35b0101a
Asia-Arif <asiarajpoot9320@gmail.com>
2026-08-25 16:08:41 +0500

    some changes

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import { BASE_URL } from "../../backend/config/envConfig";
+
 const Login = () => {
     const navigate = useNavigate();
 
@@ -27,7 +27,7 @@ const Login = () => {
 
         try {
             const response = await fetch(
-                "BASE_URL/api/auth/login",
+                "http://localhost:5000/api/auth/login",
                 {
                     method: "POST",
                     headers: {
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,6 +1,5 @@
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import { BASE_URL } from "../../backend/config/envConfig";
 const Register = () => {
     const navigate = useNavigate();
 
@@ -28,7 +27,7 @@ const Register = () => {
 
         try {
             const response = await fetch(
-                "BASE_URL/api/auth/register",
+                "http://localhost:5000/api/auth/register",
                 {
                     method: "POST",
                     headers: {
